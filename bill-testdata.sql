cd46b9b4377c2d1628adffbd8af0831e086b2c0d
Alex <swoec.wang@gmail.com>
2018-12-02 21:16:09 +1300

    upload test data

---
+++ bill-testdata.sql
@@ -0,0 +1,23 @@
+/*
+-- Query: SELECT * FROM energe.API_bill
+LIMIT 0, 1000
+
+-- Date: 2018-12-02 19:04
+*/
+INSERT INTO `API_bill` (`id`,`icp_id`,`direction`,`interval_read`,`read_timestamp`) VALUES (1,33,'X',1.3430,'2018-06-14 00:30:00.000000');
+INSERT INTO `API_bill` (`id`,`icp_id`,`direction`,`interval_read`,`read_timestamp`) VALUES (2,33,'X',1.3440,'2018-06-15 00:30:00.000000');
+INSERT INTO `API_bill` (`id`,`icp_id`,`direction`,`interval_read`,`read_timestamp`) VALUES (3,33,'X',1.3440,'2018-06-16 00:30:00.000000');
+INSERT INTO `API_bill` (`id`,`icp_id`,`direction`,`interval_read`,`read_timestamp`) VALUES (4,33,'X',1.3440,'2018-06-17 00:30:00.000000');
+INSERT INTO `API_bill` (`id`,`icp_id`,`direction`,`interval_read`,`read_timestamp`) VALUES (5,33,'X',1.3540,'2018-06-17 00:30:00.000000');
+INSERT INTO `API_bill` (`id`,`icp_id`,`direction`,`interval_read`,`read_timestamp`) VALUES (6,33,'X',1.3570,'2018-06-18 00:30:00.000000');
+INSERT INTO `API_bill` (`id`,`icp_id`,`direction`,`interval_read`,`read_timestamp`) VALUES (7,33,'X',1.3580,'2018-06-18 00:30:00.000000');
+INSERT INTO `API_bill` (`id`,`icp_id`,`direction`,`interval_read`,`read_timestamp`) VALUES (8,33,'X',1.3580,'2018-06-18 00:30:00.000000');
+INSERT INTO `API_bill` (`id`,`icp_id`,`direction`,`interval_read`,`read_timestamp`) VALUES (9,33,'X',1.3580,'2018-06-18 00:30:00.000000');
+INSERT INTO `API_bill` (`id`,`icp_id`,`direction`,`interval_read`,`read_timestamp`) VALUES (10,33,'X',1.3580,'2018-06-18 00:30:00.000000');
+INSERT INTO `API_bill` (`id`,`icp_id`,`direction`,`interval_read`,`read_timestamp`) VALUES (11,33,'X',1.3580,'2018-06-18 00:30:00.000000');
+INSERT INTO `API_bill` (`id`,`icp_id`,`direction`,`interval_read`,`read_timestamp`) VALUES (12,33,'X',1.3580,'2018-06-18 00:30:00.000000');
+INSERT INTO `API_bill` (`id`,`icp_id`,`direction`,`interval_read`,`read_timestamp`) VALUES (13,33,'X',1.3580,'2018-06-18 00:30:00.000000');
+INSERT INTO `API_bill` (`id`,`icp_id`,`direction`,`interval_read`,`read_timestamp`) VALUES (14,34,'X',1.3580,'2018-06-15 00:30:00.000000');
+INSERT INTO `API_bill` (`id`,`icp_id`,`direction`,`interval_read`,`read_timestamp`) VALUES (15,35,'X',1.3580,'2018-06-14 00:30:00.000000');
+INSERT INTO `API_bill` (`id`,`icp_id`,`direction`,`interval_read`,`read_timestamp`) VALUES (16,36,'X',1.3580,'2018-06-15 00:30:00.000000');
+INSERT INTO `API_bill` (`id`,`icp_id`,`direction`,`interval_read`,`read_timestamp`) VALUES (17,37,'X',1.3580,'2018-06-15 10:30:00.000000');
